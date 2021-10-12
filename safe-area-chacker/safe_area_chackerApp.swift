07eaac39e285ad6862b54548571383d9867474a3
Shima <shima.jin@icloud.com>
2021-10-12 12:15:20 +0900

    Initial Commit

---
+++ safe-area-chacker/safe_area_chackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  safe_area_chackerApp.swift
+//  safe-area-chacker
+//
+//  Created by Jinsei Shima on 2021/10/12.
+//
+
+import SwiftUI
+
+@main
+struct safe_area_chackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
