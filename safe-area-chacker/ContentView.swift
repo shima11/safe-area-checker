82b68af47b4810735c1c227d3dad2da71b69aae3
shima11 <shima.jin@icloud.com>
2022-02-22 14:48:53 +0900

    update

--- safe-area-chacker/ContentView.swift
+++ safe-area-chacker/ContentView.swift
@@ -25,8 +25,9 @@ struct ContentView: View {
             //          Text(String("local frame:\(proxy.frame(in: .local))"))
             Text(String("frame:\(proxy.frame(in: .global))"))
             Text(String("screen size:\(UIScreen.main.bounds)"))
-            Text(String("safe area insets:\(proxy.safeAreaInsets)"))
             Text(String("status bar height:\(getStatusBarHeight())"))
+            Text(String("safe area insets:\(proxy.safeAreaInsets)"))
+            Text(String("window safe area:\(getWindowSafeArea())"))
           }
           .padding(.horizontal, 24)
           Spacer()
@@ -42,6 +43,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+      .extendSafeArea(additionalSafeAreaInsets: .init(top: 0, left: 0, bottom: 100, right: 0))
   }
 }
 
@@ -57,11 +59,54 @@ func getSystemInfo() -> String {
     .joined(separator: ",")
 }
 
-func getStatusBarHeight() -> CGFloat{
+func getStatusBarHeight() -> CGFloat {
   if #available(iOS 13.0, *) {
     let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
     return window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
   } else {
     return UIApplication.shared.statusBarFrame.height
+  }
+}
+
+func getWindowSafeArea() -> UIEdgeInsets? {
+  UIApplication.shared.keyWindow?.safeAreaInsets
+}
+
+extension View {
+  func extendSafeArea(additionalSafeAreaInsets: UIEdgeInsets) -> some View {
+    modifier(ExtendedSafeAreaModifier(additionalSafeAreaInsets: additionalSafeAreaInsets))
+  }
+}
+
+struct ExtendedSafeAreaModifier: ViewModifier {
+
+  let additionalSafeAreaInsets: UIEdgeInsets
+
+  func body(content: Content) -> some View {
+    Container(content: content, additionalSafeAreaInsets: additionalSafeAreaInsets)
+//      .edgesIgnoringSafeArea(.all)
+  }
+  private struct Container: UIViewRepresentable {
+
+    typealias UIViewType = UIView
+
+    let content: Content
+    let additionalSafeAreaInsets: UIEdgeInsets
+
+    func makeUIView(context: Context) -> UIViewType {
+      let hostingController = UIHostingController(rootView: content)
+      hostingController.additionalSafeAreaInsets = additionalSafeAreaInsets
+      context.coordinator.container = hostingController
+      return hostingController.view
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+      uiView.setNeedsLayout()
+    }
+    func makeCoordinator() -> Coordinator {
+      .init()
+    }
+    class Coordinator {
+      var container: UIViewController!
+    }
   }
 }
